1fd8d9d0ee7de195bcdf132fb3bdabcc33797afa
Aditya Chaurasia <adityachrs5@gmail.com>
2022-07-24 16:20:51 +0530

    Second Commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -18,6 +18,9 @@ public class App
         for(int i = 0; i < v; i++)
             adj[i] = new LinkedList();
     }
+    public void test(){
+        System.out.println("hello world");
+    }
 
     void addEdge(int v, int w){
         adj[v].add(w);
